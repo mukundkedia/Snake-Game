f4f3af70f46d1cf80ee457b398e48e059479b423
Mukund Kedia <mukundkedia7478@pop-os.localdomain>
2020-06-09 18:01:14 +0530

    Updates

---
+++ precache-manifest.6ba5d157a332c29da62ee46151b26225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4577bd9e69103efa25af3c349dd22dba",
+    "url": "/Snake-Game/index.html"
+  },
+  {
+    "revision": "353a970d9c0b69bab164",
+    "url": "/Snake-Game/static/css/main.4be790ad.chunk.css"
+  },
+  {
+    "revision": "c3e5603df47f4b0a4734",
+    "url": "/Snake-Game/static/js/2.bcb97bc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Snake-Game/static/js/2.bcb97bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353a970d9c0b69bab164",
+    "url": "/Snake-Game/static/js/main.d0157f07.chunk.js"
+  },
+  {
+    "revision": "3982213ce037b78921b9",
+    "url": "/Snake-Game/static/js/runtime-main.d1734b81.js"
+  }
+]);
