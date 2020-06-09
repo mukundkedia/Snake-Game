a8b1c300e9e67deba1f50d219ad6bfd5ffe2bad0
Mukund Kedia <mukundkedia7478@pop-os.localdomain>
2020-06-09 18:43:27 +0530

    all files added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,181 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import Snake from './components/Snake'
+import Food from './components/Food'
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getRandomCoordinates = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let y =  Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  return [x,y]
+}
+
+const initialState = {
+  food: getRandomCoordinates(),
+  speed: 200,
+  direction: 'RIGHT',
+  snakeDots: [
+    [0,0],
+    [2,0]
+  ]
+}
+
+class App extends Component {
+
+  state = initialState;
+
+  componentDidMount() {
+    setInterval(this.moveSnake, this.state.speed);
+    document.onkeydown = this.onKeyDown;
+  }
+
+  componentDidUpdate() {
+    this.checkIfOutOfBorders();
+    this.checkIfCollapsed();
+    this.checkIfEat();
+  }
+
+  onKeyDown = (event) => {
+    
+    switch (event.keyCode) {
+      case 38:
+        this.setState({direction: 'UP'});
+        break;
+      case 40:
+        this.setState({direction: 'DOWN'});
+        break;
+      case 37:
+        this.setState({direction: 'LEFT'});
+        break;
+      case 39:
+        this.setState({direction: 'RIGHT'});
+        break;
+    }
+  }
+
+  moveSnake = () => {
+    let dots = [...this.state.snakeDots];
+    let head = dots[dots.length - 1];
+
+    switch (this.state.direction) {
+      case 'RIGHT':
+        head = [head[0] + 2, head[1]];
+        break;
+      case 'LEFT':
+        head = [head[0] - 2, head[1]];
+        break;
+      case 'DOWN':
+        head = [head[0], head[1] + 2];
+        break;
+      case 'UP':
+        head = [head[0], head[1] - 2];
+        break;
+    }
+    dots.push(head);
+    dots.shift();
+    this.setState({
+      snakeDots: dots
+    })
+  }
+
+  checkIfOutOfBorders() {
+    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
+    if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0) {
+      this.onGameOver();
+    }
+  }
+
+  checkIfCollapsed() {
+    let snake = [...this.state.snakeDots];
+    let head = snake[snake.length - 1];
+    snake.pop();
+    snake.forEach(dot => {
+      if (head[0] == dot[0] && head[1] == dot[1]) {
+        this.onGameOver();
+      }
+    })
+  }
+
+  checkIfEat() {
+    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
+    let food = this.state.food;
+    if (head[0] == food[0] && head[1] == food[1]) {
+      this.setState({
+        food: getRandomCoordinates()
+      })
+      this.enlargeSnake();
+      this.increaseSpeed();
+    }
+  }
+
+  enlargeSnake() {
+    let newSnake = [...this.state.snakeDots];
+    newSnake.unshift([])
+    this.setState({
+      snakeDots: newSnake
+    })
+  }
+
+  increaseSpeed() {
+    if (this.state.speed > 10) {
+      this.setState({
+        speed: this.state.speed - 50
+      })
+    }
+  }
+
+  onGameOver() {
+    const score = this.state.snakeDots.length-2;
+    alert(`Game Over. Your Score is ${score}`);
+    this.setState(initialState)
+  }
+
+  rightHandler(){
+    this.setState({
+      direction:'RIGHT'
+    })
+  }
+
+  upHandler(){
+    this.setState({
+      direction:'UP'
+    })
+  }
+
+  downHandler(){
+    this.setState({
+      direction:'DOWN'
+    })
+  }
+
+  leftHandler(){
+    this.setState({
+      direction:'LEFT'
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <header>SNAKE GAME</header>
+      <div className="game-area">
+        
+        <Snake snakeDots={this.state.snakeDots}/>
+        <Food dot={this.state.food}/>
+      </div>
+      <div className="buttons">
+      <button onClick={this.upHandler.bind(this)} className="up">UP</button>
+      <button onClick={this.downHandler.bind(this)} className="down">DOWN</button>
+      <button onClick={this.leftHandler.bind(this)} className="left">LEFT</button>
+      <button onClick={this.rightHandler.bind(this)} className="right">RIGHT</button>
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
